1408a28f3e57bc3655626753ded6d5833071710c
Ineiev <ineiev@gnu.org>
2023-01-13 08:39:34 +0000

    clean up and refactor
    Anisomorphic changes:
    
    * use single unified SQL query
    * no resume and watching for squads
    * roles are explained once rather than twice
    * empty cells are filled with '&nbsp;'
    * fix i18n of strings like 'task tracker technician'

--- frontend/php/project/memberlist.php
+++ frontend/php/project/memberlist.php
@@ -26,15 +26,122 @@
 require_once ('../include/init.php');
 require_directory ("trackers");
 
-function specific_print_role ($row, $title)
+function is_squad ($m)
+{
+  return $m['admin_flags'] === 'SQD';
+}
+
+# Return lists of members sorted by the onduty flag
+# [[(active members)], [(inactive members)]].
+function fetch_member_list ($group_id)
+{
+  $flag_select = '';
+  foreach (['admin', 'bugs', 'task', 'patch', 'news', 'support'] as $f)
+    $flag_select .= " g.{$f}_flags AS {$f}_flags,";
+  $res = db_execute ("
+    SELECT
+      u.user_name, u.user_id, u.realname, u.add_date, u.people_view_skills,
+      u.email,$flag_select g.onduty AS onduty
+    FROM user u, user_group g
+    WHERE u.user_id = g.user_id AND g.group_id = ? AND g.admin_flags <> 'P'
+    ORDER BY u.user_name",
+    [$group_id]
+  );
+
+  $members = [0 => [], 1 => []];
+  while ($row = db_fetch_array ($res))
+    {
+      $od = $row['onduty']? 1: 0;
+      $members[$od][] = $row;
+    }
+  return $members;
+}
+
+# Return string listing specific roles of given member.
+function expand_roles ($m)
 {
   # TRANSLATORS: these roles are explained in
   # html.php:html_member_explain_roles.
   $roles = [
     1 => _("technician"), 3 => _("manager"), 2 => _("techn. & manager")
   ];
-  if (isset ($roles[$row]))
-    print "$title {$roles[$row]},<br />";
+  # TRANSLATORS: these strings are combined
+  # with (technician|manager|techn. & manager)
+  $trackers = ['support' => _("support tracker"), 'bugs' => _("bug tracker"),
+    'task' => _("task tracker"), 'patch' => _("patch tracker"),
+    'news' => _("news tracker")
+  ];
+  $ret = '';
+  foreach ($trackers as $art => $title)
+    {
+      $idx = $m[$art . '_flags'];
+      if (isset ($roles[$idx]))
+        $ret .= sprintf (
+          _('<!-- tracker -->%1$s <!-- role -->%2$s'), $title, $roles[$idx])
+          . "<br />\n";
+    }
+  if ($ret === '')
+    return '&nbsp;';
+  return $ret;
+}
+
+function role_cell ($m, $is_admin)
+{
+  if ($is_admin)
+    return _("project admin");
+  return expand_roles ($m);
+}
+
+function print_in_td ($s)
+{
+  print "\t\t<td align=\"middle\">$s</td>\n";
+}
+
+function role_icon ($flags, $group_id)
+{
+  global $sys_group_id;
+  if ($flags == 'A')
+    {
+      if ($group_id == $sys_group_id)
+        return ["site-admin",  _("Site Administrator")];
+      return ["project-admin", _("Project Administrator")];
+    }
+  if ($flags == 'SQD')
+    return ["people", _("Squad")];
+  return ["project-member", _("Project Member")];
+}
+
+function print_role_icon ($m, $group_id)
+{
+  list ($icon, $icon_alt) = role_icon ($m['admin_flags'], $group_id);
+
+  print "\t\t<td><span class='help' title=\"$icon_alt\">"
+    . html_image ("roles/$icon.png", ['alt' => $icon_alt, 'class' => 'icon'])
+    . "</span></td>\n";
+}
+
+function resume_cell ($m, $sys_home)
+{
+  if (is_squad ($m))
+    return '&nbsp;';
+  if ($m['people_view_skills'] != 1)
+    # TRANSLATORS: this is a label shown when user's skills
+    # are unavailable.
+    return _("Set to private");
+  return "<a href=\"{$sys_home}people/resume.php?user_id={$m['user_id']}\">"
+    . _("View Skills") . "</a>";
+}
+
+function watched_link ($this_user, $m, $group_id, $sys_home)
+{
+  if (is_squad ($m))
+    return '&nbsp;';
+  $is_watched = trackers_data_is_watched ($this_user, $m['user_id'], $group_id);
+  if ($m['user_id'] == $this_user || $is_watched)
+    return "---";
+  return "<a href=\"${sys_home}my/groups.php?"
+    . "func=addwatchee&amp;group_id=$group_id&amp;watchee_id="
+    . $m['user_id'] . "\">" . _("Watch partner") . "</a>";
 }
 
 extract (sane_import ('get', ['digits' => "detailed"]));
@@ -91,167 +198,51 @@ else
   );
 print "</p>\n";
 
-$activeness = [1, 0];
-foreach ($activeness as $active)
+member_explain_roles ();
+print '<p>'
+  . _("On this page are only presented specific roles, roles\nwhich "
+      . "are not attributed by default when joining this project.")
+  . "</p>\n";
+
+$members = fetch_member_list ($group_id);
+$title_arr = ["&nbsp;", _("Member")];
+if ($detailed)
+  $title_arr[] = _("Specific Role");
+$title_arr[] = _("Resume and Skills");
+if ($approved_member)
+  $title_arr[] = _("Watch");
+
+$duty_titles = [
+  _('Currently inactive members'),  _('Active members on duty')
+];
+foreach ([1, 0] as $onduty)
   {
-    if ($detailed)
-      {
-        $res_memb = db_execute ("
-          SELECT
-            user.user_name AS user_name, user.user_id AS user_id,
-          user.realname AS realname, user.add_date AS add_date,
-          user.people_view_skills AS people_view_skills,
-          user_group.admin_flags AS admin_flags,
-          user_group.bugs_flags AS bugs_flags,
-          user_group.task_flags AS task_flags,
-          user_group.patch_flags AS patch_flags,
-          user_group.news_flags AS news_flags,
-          user_group.support_flags AS support_flags, user.email AS email
-          FROM user, user_group
-          WHERE
-            user.user_id = user_group.user_id AND user_group.group_id = ?
-            AND user_group.admin_flags <> 'P' AND user_group.onduty = ?
-          ORDER BY user.user_name",
-          [$group_id, $active]
-        );
-        member_explain_roles ();
-        # FIXME: yeupou--gnu.org 2003-11-07
-        # The best would be to print non-specific roles but roles in any case.
-        # It requires more, so we will see if there are people interested
-        # in that or not.
-        print '<p>'
-          . _("On this page are only presented specific roles, roles\nwhich "
-              . "are not attributed by default when joining this project.")
-          . "</p>\n";
-      }
-    else
-      $res_memb = db_execute ("
-        SELECT
-          user.user_name AS user_name, user.user_id AS user_id,
-          user.realname AS realname, user.add_date AS add_date,
-          user.people_view_skills AS people_view_skills,
-          user_group.admin_flags AS admin_flags, user.email AS email
-        FROM user, user_group
-        WHERE
-          user.user_id = user_group.user_id AND user_group.group_id = ?
-          AND user_group.admin_flags <> 'P' AND user_group.onduty = ?
-        ORDER BY user.user_name",
-        [$group_id, $active]
-      );
-    $title_arr = ["&nbsp;", _("Member")];
-    if ($detailed)
-      $title_arr[] = _("Specific Role");
-    # yeupou--gnu.org, 2004-11-04, remove email from this page; this data
-    # is accessible elsewhere, via links. It saves us extra tests on whether
-    # users want to hide their email or not.
-    $title_arr[] = _("Resume and Skills");
-    if ($approved_member)
-      $title_arr[] = _("Watch");
-
-    if  (db_numrows ($res_memb) == 0)
-      continue;
-
-    if ($active)
-      print '<h2>' . _('Active members on duty') . "</h2>\n";
-    else
-      print '<h2>' . _('Currently inactive members') . "</h2>\n";
+    print "<h2>{$duty_titles[$onduty]}</h2>\n";
     echo html_build_list_table_top ($title_arr);
 
-    $i = 1;
-    while ($row_memb = db_fetch_array ($res_memb))
+    $mem = $members[$onduty];
+    $cnt = count ($mem);
+    for ($i = 0; $i < $cnt; $i++)
       {
-        if ($row_memb['admin_flags'] != 'P')
-          {
-            $i++;
-            $color = utils_altrow ($i);
-            if ($row_memb['admin_flags'] == 'A')
-              $color = "boxhighlight";
-            print "\n\t<tr class=\"$color\">\n";
-            print "\t\t";
+        $m = $mem[$i];
+        $color = utils_altrow ($i);
+        $is_admin = $m['admin_flags'] == 'A';
+        if ($is_admin)
+          $color = "boxhighlight";
+        print "\n\t<tr class=\"$color\">\n";
+        print_role_icon ($m, $group_id);
+        print "\t\t<td>" . utils_user_link ($m['user_name'], $m['realname'])
+          . "</td>\n";
+        if ($detailed)
+          print_in_td (role_cell ($m, $is_admin));
 
-            if ($row_memb['admin_flags'] == 'A')
-              {
-                if ($group_id != $sys_group_id)
-                  {
-                    $icon = "project-admin";
-                    $icon_alt = _("Project Administrator");
-                  }
-                else
-                  {
-                    $icon = "site-admin";
-                    $icon_alt = _("Site Administrator");
-                  }
-              }
-            elseif ($row_memb['admin_flags'] == 'SQD')
-              {
-                $icon = "people";
-                $icon_alt = _("Squad");
-              }
-            else
-              {
-                $icon = "project-member";
-                $icon_alt = _("Project Member");
-              }
-
-            print "\t\t<td><span class='help' title=\"$icon_alt\">"
-              . html_image (
-                  "roles/$icon.png", ['alt' => $icon_alt, 'class' => 'icon']
-                )
-              . "</span></td>\n<td>"
-              . utils_user_link ($row_memb['user_name'], $row_memb['realname'])
-              . "</td>\n";
-            if ($detailed)
-              {
-                print "\t\t<td align=\"middle\">";
-                if ($row_memb['admin_flags'] == 'A')
-                  print _("project admin");
-                else
-                  foreach (
-                    [
-                      'support_flags' => _("support tracker"),
-                      'bugs_flags' => _("bug tracker"),
-                      'task_flags' => _("task tracker"),
-                      'patch_flags' => _("patch tracker"),
-                      'news_flags' => _("news tracker")
-                    ] as $idx => $val
-                  )
-                    specific_print_role ($row_memb[$idx], $val);
-                print "</td>\n";
-              }
-
-            print "\t\t<td align='middle'>";
-            if ($row_memb['people_view_skills'] == 1)
-              print "<a href=\"$sys_home"
-                . "people/resume.php?user_id={$row_memb['user_id']}\">"
-                . _("View Skills") . "</a>";
-            else
-              # TRANSLATORS: this is a label shown when user's skills
-              # are unavailable.
-              print _("Set to private");
-            print "</td>\n";
-          # Watch
-           if ($approved_member)
-             {
-               $is_watched = trackers_data_is_watched (
-                 $this_user, $row_memb['user_id'], $group_id
-               );
-               print "\t\t<td align='middle'>";
-               if ($row_memb['user_id'] != $this_user && !$is_watched)
-                  # Permit to add a watchee only if not already in the watched
-                  # list.
-                  print "<a href=\"${sys_home}my/groups.php?"
-                    . "func=addwatchee&amp;group_id=$group_id&amp;watchee_id="
-                    . $row_memb['user_id'] . "\">" . _("Watch partner")
-                    . "</a>";
-                else
-                  print "---";
-                print "</td>\n";
-              }
-            print "\t<tr>\n";
-          }
+        print_in_td (resume_cell ($m, $sys_home));
+        if ($approved_member)
+          print_in_td (watched_link ($this_user, $m, $group_id, $sys_home));
+        print "\t<tr>\n";
       }
     print "\t</table>\n";
-  } # foreach ($activeness as $active)
+  } # foreach ([1, 0] as $onduty)
 
 if ($project->getGPGKeyring ())
   {
