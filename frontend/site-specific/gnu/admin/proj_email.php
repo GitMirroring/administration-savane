9623f4b1d714d4cb64e0233f844a6ba06a2c390b
Ineiev <ineiev@gnu.org>
2023-03-14 09:23:35 +0000

    update text, provide fallbacks for running locally

--- frontend/site-specific/gnu/admin/proj_email.php
+++ frontend/site-specific/gnu/admin/proj_email.php
@@ -1,11 +1,11 @@
 <?php
-# Savannah - Project registration STEP 6 Confirmation mail.
+# Savannah - group registration step 6, Confirmation mail.
 # Here, you can configure the mail sent to user and admins.
 
 # Copyright (C) 1999-2000 The SourceForge Crew
 # Copyright (C) 2002 Mathieu Roy <yeupou--gnu.org>
 # Copyright (C) 2013 Karl Berry
-# Copyright (C) 2017, 2022 Ineiev <ineiev@gnu.org>
+# Copyright (C) 2017, 2022, 2023 Ineiev <ineiev@gnu.org>
 #
 # This file is part of Savane.
 #
@@ -22,46 +22,43 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# We include this as function, it's easier to work with vars
-# in this way.
+function approval_message_fmt ()
+{
+  # This message is not localized because it's sent to admins.
+  return "\n"
+    . "Your group has been approved on %s.\n"
+    . "Group full name:   %s\n"
+    . "Group system name: %s\n"
+    . "Group page:        %s\n"
+    . "\n"
+    . "Please note, that it will take up to half an hour for the system to\n"
+    . "be updated (CVS repository creation for instance) before your group\n"
+    . "will be fully functional.\n"
+    . "\n"
+    . "Enjoy the system, and please tell others about %s.\n"
+    . "Let us know if there is anything we can do to help you.\n"
+    . "\n"
+    . " -- the %s volunteers\n"
+    . "\n";
+}
 
-# This message is not localized because it's sent to admins.
-
+# We include this as function, it's easier to work with vars this way.
 function approval_gen_email ($group_name, $unix_group_name)
 {
-  $fmt = ('
-Your project registration for %s has been approved.
-Project full name:   %s
-Project system name: %s
-Project page:        %s
+  global $sys_name, $sys_https_host;
 
-Please note, that it will take up to half an hour for the system to
-be updated (CVS repository creation for instance) before your project
-will be fully functional.
-
-Enjoy the system, and please tell others about %s.
-Let us know if there is anything we can do to help you.
-
- -- the %s Volunteers
-
-
-
-
-
-Post scriptum, important note:
-   In order to release your project, you should write copyright notices
-   and license notices at the beginning of every source code file, and
-   include a copy of the plain text version of the license. If your
-   software is published under the GNU GPL, please read
-   https://www.gnu.org/licenses/gpl.html.
-');
-  $forge = $GLOBALS['sys_name'];
-  $host = $GLOBALS['sys_https_host'];
-  $message =
+  if (empty ($sys_name))
+    $forge = '<unknown>';
+  else
+    $forge = $sys_name;
+  if (empty ($sys_https_host))
+    $host = '<no HTTPS host>';
+  else
+    $host = $sys_https_host;
+  return
     sprintf (
-      $fmt, $forge, $group_name, $unix_group_name,
+      approval_message_fmt (), $forge, $group_name, $unix_group_name,
       "https://$host/projects/$unix_group_name", $forge, $forge
     );
-  return $message;
 }
 ?>
