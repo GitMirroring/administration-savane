c06c66fdb9baaeae3dcd5dfbeb188c35c43bb557
Ineiev <ineiev@gnu.org>
2024-03-26 07:05:04 +0000

    improve messages; drop dead code

--- frontend/php/siteadmin/user_changepw.php
+++ frontend/php/siteadmin/user_changepw.php
@@ -58,16 +58,13 @@ form_check ('update');
 
 $error = '';
 
-# Check for valid register from form post.
-function register_valid ()
+# Return 1 if inputs are valid, else set $error and return 0.
+function validate_inputs ()
 {
-  global $update, $user_id, $error, $form_pw, $form_pw2;
+  global $update, $error, $form_pw, $form_pw2;
 
   if (!$update)
     return 0;
-  # Check against old password.
-  db_execute ("SELECT user_pw FROM user WHERE user_id = ?", [$user_id]);
-
   if (!$form_pw)
     {
       $error = no_i18n ('no password provided');
@@ -83,28 +80,26 @@ function register_valid ()
       $error = no_i18n ('provided password is considered weak');
       return 0;
     }
-
-  # If we got this far, it must be good.
-  db_autoexecute ('user', ['user_pw' => account_encryptpw ($form_pw)],
-    DB_AUTOQUERY_UPDATE, "user_id = ?", [$user_id]
-  );
   return 1;
 }
 
-$title = sprintf (no_i18n ('Change Password for %s'), user_getname ($user_id));
+$title = sprintf (no_i18n ('Change password for %s'), user_getname ($user_id));
 
 $HTML->header (['title' => $title]);
+print html_h (2, no_i18n ('Savannah Password Change'));
 # Check for valid login, if so, congratulate.
-if (register_valid ())
+if (validate_inputs ())
   {
-    print "\n<p><strong>"
-      . no_i18n ('Savannah Change Confirmation') . "</strong></p>\n<p>"
-      . no_i18n ("Congratulations. You have managed to change this user's"
+    # If we got this far, it must be good.
+    db_autoexecute ('user', ['user_pw' => account_encryptpw ($form_pw)],
+      DB_AUTOQUERY_UPDATE, "user_id = ?", [$user_id]
+    );
+    print "<p>"
+      . no_i18n ("Congratulations. You have managed to change this user's\n"
           . "password.")
       . "</p>\n<p>";
     printf (
-      no_i18n ('You should now <a href="%s">Return to UserList</a>.'),
-      '/admin/userlist.php'
+      no_i18n ('<a href="%s">Return to user list</a>.'), '/admin/userlist.php'
     );
     print "</p>\n";
   }
@@ -114,8 +109,7 @@ else
       $error = '<p><b>' . no_i18n ('Password change failed')
         . ": $error</b></p>\n";
 
-    print html_h (2, no_i18n ('Savannah Password Change'))
-      . "\n$error" . form_header () . '<p>'
+    print "$error" . form_header () . '<p>'
       . html_label ('form_pw', no_i18n ('New Password:'))
       . "<br />\n" . form_input ('password', 'form_pw') . "\n</p>\n<p>"
       . html_label ('form_pw2', no_i18n ('New Password (repeat):'))
