ddbdb711458482706ff90ba403d617d82a4e04b5
Ineiev <ineiev@gnu.org>
2024-06-21 07:20:13 +0000

    list $sys_upload_dir

--- frontend/php/testconfig.php
+++ frontend/php/testconfig.php
@@ -715,7 +715,7 @@ function output_sysvars ()
   $variables = [
     'dbhost', 'dbname', 'dbpasswd', 'dbport', 'dbsocket', 'dbuser',
     'default_domain', 'etc_dir', 'file_domain', 'graphviz', 'https_host',
-    'incdir', 'url_topdir', 'www_topdir',
+    'incdir', 'upload_dir', 'url_topdir', 'www_topdir',
     'linguas', 'localedir',
     'mail_admin', 'mail_domain', 'mail_replyto', 'name', 'reply_to',
     'themedefault', 'unix_group_name', 'upload_max',
