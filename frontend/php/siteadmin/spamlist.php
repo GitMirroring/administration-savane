1abdc3ebf422a3139938debe6f246a25a18f1f10
Ineiev <ineiev@gnu.org>
2024-10-01 06:20:48 +0000

    refactor siteadmin/spamlist.php

--- frontend/php/siteadmin/spamlist.php
+++ frontend/php/siteadmin/spamlist.php
@@ -48,31 +48,82 @@ session_require (['group' => '1', 'admin_flags' => 'A']);
 # We don't internationalize messages in this file because they are
 # for Savannah admins who use English.
 
-extract (sane_import ('get',
-  ['digits' => ['wash_user_id', 'max_rows', 'offset']]
-));
+extract (sane_import ('get', ['digits' => ['max_rows', 'offset']]));
 
-if ($wash_user_id)
-  {
-    if (!user_exists ($wash_user_id))
+function wash_user ()
+{
+  extract (sane_import ('get', ['digits' => ['wash_user_id']]));
+  if (empty ($wash_user_id))
+    return;
+  if (!user_exists ($wash_user_id))
+    {
       fb (sprintf (no_i18n ("User #%s not found."), $wash_user_id), 1);
-    else
-      {
-        # Update the user spamscore field.
-        db_execute (
-          "UPDATE user SET spamscore = '0' WHERE user_id = ?", [$wash_user_id]
-        );
-        # Previous comment flagged as spam will stay as such.
-        # We just changed the affected user id that it won't affect this guy
-        # any more.
-        # We assume that message flagged as spam really were.
-        # (we may change that in the future, depending on user experience).
-        db_execute ("
-          UPDATE trackers_spamscore SET affected_user_id = '100'
-          WHERE affected_user_id = ?", [$wash_user_id]
-        );
-      }
-  }
+      return;
+    }
+  db_execute (
+    "UPDATE user SET spamscore = '0' WHERE user_id = ?", [$wash_user_id]
+  );
+  # The comment flagged as spam will stay as such.  We assume that the messages
+  # flagged as spam should stay hidden, we just change the record of the user
+  # (for example, the admins of the tracker may hide duplicate posts as spam).
+  db_execute ("
+    UPDATE trackers_spamscore SET affected_user_id = '100'
+    WHERE affected_user_id = ?", [$wash_user_id]
+  );
+}
+
+function format_user_data ($entry, $i)
+{
+  global $php_self;
+
+  return '<td width="25%">'
+    . utils_user_link ($entry['user_name'], $entry['realname'])
+    . "</td>\n<td width='5%' class='center'>{$entry['spamscore']}</td>\n"
+    . "</td>\n<td width='5%' class='center'>"
+    . utils_link ("$php_self?wash_user_id={$entry['user_id']}#users_results",
+        html_image ('bool/ok.png', ['alt=' => no_i18n ("Wash score")])
+      )
+    . "</td>\n";
+}
+
+function fetch_incriminated_posts ($user_id)
+{
+  return db_execute ("
+    SELECT t.artifact, t.item_id, t.comment_id, u.user_name
+    FROM trackers_spamscore t, user u
+    WHERE t.affected_user_id = ? AND u.user_id = t.reporter_user_id
+    LIMIT 50", [$user_id]
+  );
+}
+
+function get_incriminated_posts ($user_id)
+{
+  global $sys_home;
+  $flagged_by = $incriminated_posts = [];
+  $res = fetch_incriminated_posts ($user_id);
+  while ($ent = db_fetch_array ($res))
+    {
+      $flagged_by[$ent['user_name']] = true;
+      $idx = $ent['artifact'] . $ent['item_id'] . 'C' . $ent['comment_id'];
+      $incriminated_posts[$idx] = utils_link (
+        $sys_home . $ent['artifact'] . '/?item_id=' . $ent['item_id']
+        . '&amp;func=viewspam&amp;comment_internal_id='
+        . $ent['comment_id'] . '#spam' . $ent['comment_id'],
+        no_i18n ("here")
+      );
+    }
+  return [array_keys ($flagged_by), $incriminated_posts];
+}
+
+function format_flagged_by ($flagged_by)
+{
+  $ret = [];
+  foreach ($flagged_by as $user)
+    $ret[] = utils_user_link ($user);
+  return join (', ', $ret);
+}
+
+wash_user ();
 
 site_admin_header (
   ['title' => no_i18n ("Monitor Spam"), 'context' => 'admhome']
@@ -123,50 +174,12 @@ while ($entry = db_fetch_array ($result))
     if ($i > $max_rows)
       break;
 
-    $res_score = db_execute ("
-      SELECT t.artifact, t.item_id, t.comment_id, u.user_name
-      FROM trackers_spamscore t, user u
-      WHERE t.affected_user_id = ?  AND u.user_id = t.reporter_user_id
-      LIMIT 50", [$entry['user_id']]
-    );
-    $flagged_by = '';
-    $incriminated_posts = '';
-    $seen_before = [];
-    while ($ent = db_fetch_array ($res_score))
-      {
-        if (!isset ($seen_before[$ent['user_name']]))
-          {
-            $flagged_by .= utils_user_link ($ent['user_name']) . ', ';
-            $seen_before[$ent['user_name']] = true;
-          }
-        $idx = $ent['artifact'] . $ent['item_id'] . 'C' . $ent['comment_id'];
-        if (!isset ($seen_before[$idx]))
-          {
-            # Only put the string "here" for each item, otherwise it gets
-            # overlong when we have to tell comment #nnn of item #nnnn.
-            $incriminated_posts .= utils_link (
-              $sys_home . $ent['artifact'] . '/?item_id=' . $ent['item_id']
-              . '&amp;func=viewspam&amp;comment_internal_id='
-              . $ent['comment_id'] . '#spam' . $ent['comment_id'],
-              no_i18n ("here")
-            );
-            $incriminated_posts .= ', ';
-            $seen_before[$idx] = true;
-          }
-      }
-    $flagged_by = rtrim ($flagged_by, ', ');
-    $incriminated_posts = rtrim ($incriminated_posts, ', ');
-
+    list ($flagged_by, $incriminated_posts) =
+      get_incriminated_posts ($entry['user_id']);
     print '<tr class="' . utils_altrow ($i) . '">';
-    print '<td width="25%">'
-      . utils_user_link ($entry['user_name'], $entry['realname'])
-      . "</td>\n<td width='5%' class='center'>{$entry['spamscore']}</td>\n"
-      . "</td>\n<td width='5%' class='center'>"
-      . utils_link ("$php_self?wash_user_id={$entry['user_id']}#users_results",
-          html_image ('bool/ok.png', ['alt=' => no_i18n ("Wash score")])
-        )
-      . "</td>\n<td width='30%'>$incriminated_posts</td>\n"
-      . "<td width='30%'>$flagged_by</td>\n</tr>\n";
+    print format_user_data ($entry, $i);
+    print "<td width='30%'>" . join (', ', $incriminated_posts) . "</td>\n"
+      . "<td width='30%'>" . format_flagged_by ($flagged_by) . "</td>\n</tr>\n";
   }
 print "</table>\n";
 
