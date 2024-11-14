650df6c5046debf8cf7895086cd8836428fd8cac
Ineiev <ineiev@gnu.org>
2024-11-14 14:15:35 +0000

    Revert "add fundraiser"
    This reverts commit 3b2d14c20843b772cf8e9afecbe3f4f54a06f7ab.

--- frontend/php/include/layout.php
+++ frontend/php/include/layout.php
@@ -238,12 +238,6 @@ class Layout extends savane_error
     $this->generic_header_end ($params);
 
     print "\n<body>\n<div class='realbody'>\n";
-    print '<iframe title="FSF Fundraiser Banner"'
-      . 'src="//static.gnu.org/nosvn/banners/202411fundraiser/"'
-      . 'scrolling="no"'
-      . 'style="width: 100%; height: 300px; display: block; '
-      . 'margin: 0; border: 0 none; overflow: hidden;">'
-      . '</iframe>';
     sitemenu ($params);
     print "<div id='top' class='main'>\n";
     pagemenu ($params);
