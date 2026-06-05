4af087b80525dc8074af5dc110fadbe951add6ec
Ineiev <ineiev@gnu.org>
2026-06-05 14:13:42 +0000

    drop exception in privileges check, HCK-002

--- frontend/php/siteadmin/triggercreation.php
+++ frontend/php/siteadmin/triggercreation.php
@@ -47,9 +47,7 @@
 require_once ('../include/init.php');
 require_once ('../include/proj_email.php');
 
-# Skip admin rights check if we are dealing with the sys group.
-if ($sys_group_id != $group_id)
-  session_require (['group' => SESSION_ADMIN_GROUP]);
+session_require (['group' => SESSION_ADMIN_GROUP]);
 
 # Configure the group according to group type settings.
 # If a group can use a feature for its group type, assume he would
